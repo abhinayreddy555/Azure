a2d608787b0b4e9c6f73267fcd285d38c57c2540
abhinayreddy555 <35317166+abhinayreddy555@users.noreply.github.com>
2018-01-22 21:24:49 -0500

    testing

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,6 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Starting Line Changes</h1>'
+    template: '<h1>Hey Abhi your App is successfully Deployed to Azure</h1>'
 })
 export class AppComponent { }
